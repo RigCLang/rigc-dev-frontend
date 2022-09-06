df714028b07f81c400c36a8961ea6652dfcea2ce
Daniel Zaradny <danielzaradny@gmail.com>
2022-09-06 19:39:11 +0200

    refactor: don't use DetailsList in CallstackWindow

--- src/components/Callstack.tsx
+++ src/components/Callstack.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import {
-	DetailsList,
-	IColumn,
-	buildColumns,
-} from '@fluentui/react/lib/DetailsList';
-
 export type CallstackItem = {
 	functionName: string;
 	file: string;
@@ -16,71 +10,28 @@ export type CallstackProps = {
 	content: CallstackItem[];
 }
 
-interface IDetailsListDragDropExampleState {
-	items: CallstackItem[];
-	columns: IColumn[];
-	isColumnReorderEnabled: boolean;
-}
-
-const callstackColumnNames = {
-	'functionName': 'Function',
-	'file': 'File',
-	'line': 'Line',
-};
-
 interface ICallstackWindowProps {
 	className?: string;
+	callStack: CallstackItem[];
 }
 
-export class CallstackWindow extends React.Component<ICallstackWindowProps, IDetailsListDragDropExampleState> {
-	constructor(props: ICallstackWindowProps) {
-		super(props);
-
-		const items: CallstackItem[] = [
-			{
-				functionName: 'functionName',
-				file: 'file',
-				line: 1
-			},
-			{
-				functionName: 'functionName2',
-				file: 'file',
-				line: 1
-			},
-			{
-				functionName: 'functionName3',
-				file: 'file',
-				line: 1
-			},
-			{
-				functionName: 'functionName4',
-				file: 'file',
-				line: 1
-			},
-		];
-
-		this.state = {
-			items: items,
-			columns: buildColumns(items, true).map((column) => ({ ...column, name: callstackColumnNames[column.key as keyof typeof callstackColumnNames] })),
-			isColumnReorderEnabled: true,
-		};
-	}
-
+export class CallstackWindow extends React.Component<ICallstackWindowProps> {
 	public render(): JSX.Element {
-		const { items, columns } = this.state;
+		const { className, callStack } = this.props;
 
 		return (
-			<div className={this.props.className}>
-				<DetailsList
-					setKey="items"
-					compact
-					items={items}
-					columns={columns}
-					selectionPreservedOnEmptyClick={true}
-					ariaLabelForSelectionColumn="Toggle selection"
-					ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-					checkButtonAriaLabel="select row"
-				/>
+			<div className={className}>
+				<ul>
+					{
+						callStack.map(item => ( 
+							<li style={{ listStyleType: 'none' }}>
+								<span>{ item.functionName + ' ' }</span>
+								<span>{ item.file + ' ' }</span>
+								<span>{ item.line + ' ' }</span>
+							</li>
+						))
+					}
+				</ul>
 			</div>
 		);
 	}
